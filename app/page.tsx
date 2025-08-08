b8159de282b406060d019e87f468de6526a49695
Raymond Shiner <raymondshiner@gmail.com>
2025-08-08 15:46:21 -0700

    chore: remove final line from welcome blurb

--- app/page.tsx
+++ app/page.tsx
@@ -34,7 +34,7 @@ export default function Home() {
       <p className="description">
         Welcome to Shiner Systems, an independent software consulting company 
         founded and operated by Raymond Shiner. I deliver innovative software 
-         solutions tailored to meet the unique needs of my clients. Minor deployment test change.
+         
       </p>
     </main>
   )
